3ad269bd228888a23d13b3b00ae4c4afdcbb04c6
dr3x_0 <szymon.kojek@wp.pl>
2023-10-07 22:48:59 +0200

    .

--- volonteer_link/lib/main.dart
+++ volonteer_link/lib/main.dart
@@ -58,23 +58,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    final List<String> drawerTabs = [
-      "O nas",
-      "Informacja kontaktowa",
-      "Wydażenia",
-      "Chat",
-    ];
-    //double dHeight = MediaQuery.of(context).size.height;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(156, 196, 15, 227),
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-=======
     //double dHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
       home: Scaffold(
@@ -82,73 +65,10 @@ class _MyAppState extends State<MyApp> {
           title: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget> [
->>>>>>> ec2d5741192dc7ea470682d29a320e8c8e3d80d0
               //Image.asset('assets/images/logo.png',
               Column(
                 children: [
                   Center(
-<<<<<<< HEAD
-                      child: Column(
-                    children: [
-                      Text(
-                        'Volonteerly',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        'Twój Wolontariat w zasięgu ręki',
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 80),
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ))
-                ],
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            IconButton(
-                onPressed: () {}, //dodać funkcję
-                icon: const Icon(Icons.person)),
-          ],
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              const SizedBox(
-                child: Text(
-                  'Navigacja',
-                  style: TextStyle(
-                    fontSize: 26,
-                  ),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ListView(
-                    children: drawerTabs
-                        .map(
-                          (e) => Text(
-                            e,
-                            style: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        body: const Rejestracja(),
-=======
                     child: Column(
                       children:[
                         Text('Volonteerly', 
@@ -222,7 +142,6 @@ class _MyAppState extends State<MyApp> {
         drawer: Drawer(
             ),
         body: const Text('body'),
->>>>>>> ec2d5741192dc7ea470682d29a320e8c8e3d80d0
       ),
     );
   }
