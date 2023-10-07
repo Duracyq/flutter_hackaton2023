b7a22b61dfaafbf88a8a00c506beb0df42fada78
Fortland2018 <adamkowalskipl68@gmail.com>
2023-10-07 20:26:49 +0200

    .

--- volonteer_link/lib/main.dart
+++ volonteer_link/lib/main.dart
@@ -15,10 +15,85 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final List<String> drawerTabs = [
+      "O nas",
+      "Informacja kontaktowa",
+      "Wydażenia",
+      "Chat",
+    ];
+    //double dHeight = MediaQuery.of(context).size.height;
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Rejestracja(),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(156, 196, 15, 227),
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //Image.asset('assets/images/logo.png',
+              Column(
+                children: [
+                  Center(
+                      child: Column(
+                    children: [
+                      Text(
+                        'Volonteerly',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Twój Wolontariat w zasięgu ręki',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 80),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ))
+                ],
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {}, //dodać funkcję
+                icon: const Icon(Icons.person)),
+          ],
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              const SizedBox(
+                child: Text(
+                  'Navigacja',
+                  style: TextStyle(
+                    fontSize: 26,
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ListView(
+                    children: drawerTabs
+                        .map(
+                          (e) => Text(
+                            e,
+                            style: const TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: const Rejestracja(),
       ),
     );
   }
