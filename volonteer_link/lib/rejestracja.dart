01bfe13cbea8ecf674fe66ee0a890d8f616feb09
Fortland2018 <adamkowalskipl68@gmail.com>
2023-10-07 12:59:12 +0200

    rejestracja.dart

---
+++ volonteer_link/lib/rejestracja.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Rejestracja extends StatefulWidget {
+  const Rejestracja({super.key});
+
+  @override
+  State<Rejestracja> createState() => _RejestracjaState();
+}
+
+class _RejestracjaState extends State<Rejestracja> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
