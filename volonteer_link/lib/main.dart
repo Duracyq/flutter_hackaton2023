ec1c242f99aea5c984a080afb3bdf95236b9f899
dr3x_0 <szymon.kojek@wp.pl>
2023-10-07 21:58:49 +0200

    kajak

--- volonteer_link/lib/main.dart
+++ volonteer_link/lib/main.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' show json;
+import 'package:flutter/services.dart' show rootBundle;
+
 
 void main() {
   runApp(const MyApp());
 }
+
+class Event {
+  final int id;
+  final String title;
+  final String desc;
+  final String organizator;
+
+  Event({
+    required this.id,
+    required this.title,
+    required this.desc,
+    required this.organizator,
+  });
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      id: json['id'],
+      title: json['title'],
+      desc: json['desc'],
+      organizator: json['organizator'],
+    );
+  }
+}
+
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -12,93 +39,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late List<Event> events;
+
+  Future<void> loadJsonData() async {
+    String jsonData = await rootBundle.loadString('assets/data.json');
+    List<dynamic> jsonList = json.decode(jsonData);
+
+    events = jsonList.map((json) => Event.fromJson(json)).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadJsonData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //double dHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              //Image.asset('assets/images/logo.png',
-              Column(
-                children: [
-                  Center(
-                    child: Column(
-                      children:[
-                        Text('Volonteerly', 
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          ),
-                        ),
-                        Text('Twój Wolontariat w zasięgu ręki', 
-                          style: TextStyle(
-                            color: Color.fromRGBO(255,255,255,80),
-                            fontSize: 12,
-                            
-                          ),
-                      ),
-                      ],
-                    )
-                  )
-                  
-                ],
-              ),
-            ],
-          ), 
-          actions: <Widget> [
-            IconButton(
-              onPressed: (){}, //dodać funkcję 
-              icon: const Icon(Icons.person)
-            ),
-          ],
-          
-          //backgroundColor:Color.fromARGB(193, 71, 212),
-          // title: Row(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   children: [
-          //     IconButton(
-          //         onPressed: (){}, //dodać funkcję 
-          //         icon: const Icon(Icons.view_headline), //dodać icon trzech kresek
-          //         //color:, 
-          //     ),
-          //     SizedBox(
-          //       height: dHeight/6,
-          //       child: Align(
-          //         child: const Row(
-          //           children:[
-          //           Column(
-          //             children:[
-          //               Text('Volonteerly', style: TextStyle(
-          //                 color: Colors.white,
-          //                 fontSize: 24,
-          //                 ),
-          //               ),
-          //               Text('Twój Wolontariat w zasięgu ręki', style: TextStyle(
-          //                 color:Color.fromRGBO(255, 255, 255, 1),
-          //                 fontSize: 16,
-          //                 ),
-          //               ),
-          //             ],
-          //           ),
-          //         ],
-          //       ),
-          //       ),
-          //     ),
-          //     IconButton(
-          //         onPressed: (){}, //dodać funkcję 
-          //         icon: const Icon(Icons.person), 
-          //         color: Colors.white, 
-          //     ),
-          //   ],
-          // ),
+          title: Text('Event List'),
         ),
-        drawer: Drawer(
-            ),
-        body: const Text('body'),
+        body: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            Event event = events[index];
+            return ListTile(
+              title: Text(event.title),
+              subtitle: Text(event.desc),
+              trailing: Text(event.organizator),
+            );
+          },
+        ),
       ),
     );
   }
 }
+
--- volonteer_link/lib/productModel.dart
+++ volonteer_link/lib/productModel.dart
@@ -10,3 +10,21 @@ class User {
     final Bool? false;
     final int? id;
 }
+
+class Event {
+  final int id;
+  final String title;
+  final String desc;
+  final String organizator;
+
+  Event({required this.id, required this.title, required this.desc, required this.organizator});
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      id: json['id'],
+      title: json['title'],
+      desc: json['desc'],
+      organizator: json['organizator'],
+    );
+  }
+}
