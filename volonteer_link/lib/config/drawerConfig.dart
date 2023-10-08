6ba24f2a915208864e5faed98f72183454aa4b35
dr3x_0 <szymon.kojek@wp.pl>
2023-10-08 10:09:33 +0200

    a

--- volonteer_link/lib/config/drawerConfig.dart
+++ volonteer_link/lib/config/drawerConfig.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 Drawer buildDrawerConfig(BuildContext context){
@@ -9,37 +11,45 @@ Drawer buildDrawerConfig(BuildContext context){
       "Użytkownicy",
       "Chat",
     ];
+    final List<IconData?> drawerIcons = [
+        null, // Placeholder for the first item with no icon
+        Icons.phone,
+        Icons.event,
+        Icons.person,
+        Icons.chat_bubble
+      ];
 
   return Drawer(
-          child: ListView(
-            children: [
-              ...drawerTabs.map(
-                (e) => ListTile(
-                  title: Text(
-                    e,
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  contentPadding: EdgeInsets.all(deviceHeight / 25),
-                ),
-              ),
-              ListTile(
-                title: Row(
-                  children: [
-                    const Icon(Icons.info),
-                    const SizedBox(width: 8), // Add some spacing
-                    Text(
-                      'Copyright 2023',
-                      style: TextStyle(
-                        fontSize: 14, // Adjust the font size as needed
-                        color: Colors.grey, // Customize the color
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+  child: ListView(
+    children: [
+      ...drawerTabs.asMap().entries.map(
+        (entry) => ListTile(
+          leading: Icon(drawerIcons[entry.key]), // Use leading for the icon
+          title: Text(
+            entry.value,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
           ),
-        );
+          contentPadding: EdgeInsets.all(deviceHeight / 25),
+        ),
+      ),
+      ListTile(
+        leading: const Icon(Icons.info),
+        title: Row(
+          children: [
+            const SizedBox(width: 8), // Add some spacing
+            Text(
+              'Copyright 2023',
+              style: TextStyle(
+                fontSize: 14, // Adjust the font size as needed
+                color: Colors.grey, // Customize the color
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
 }
